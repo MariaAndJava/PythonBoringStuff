507b586db4977d000ccb9cace1bb08a187c7976d
MariaAndJava <muszyns@cs.uni-bonn.de>
2019-11-05 12:38:19 +0100

    worked

---
+++ RangeLoop.py
@@ -0,0 +1,5 @@
+def rangeLoop(start, end, step):
+    smth = 0
+    for i in range(start, end, step):  # start,end,step
+        smth = smth + i
+        print(smth)
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import HelloAndNameInput
 import WhileBreakContinue
+import RangeLoop
+import random
+
 
 while True:
-    print('Choose the program to be started. Type 1 or 2')
+    print('Choose the program to be started. Type smth from 1 to 4')
     choice = input()
     if int(choice) == 1:
         HelloAndNameInput.helloAndName()
@@ -10,5 +13,9 @@ while True:
     elif int(choice) == 2:
         WhileBreakContinue.start()
         break
+    elif int(choice) == 3:
+        RangeLoop.rangeLoop(4,10,2)
+    elif int(choice) == 4:
+        print(random.randint(1,100))
     else:
         print('wrong')
