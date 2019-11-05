7c2bba7dda956063f682c29335a44b73e68c09db
MariaAndJava <muszyns@cs.uni-bonn.de>
2019-11-05 11:34:49 +0100

    if-else example

--- HelloWorld.py
+++ HelloWorld.py
@@ -2,8 +2,11 @@
 print('Hello world!')
 print('What is your name?') #ask for the name
 myName = input()
-print('This is a very nice name, '+ myName)
+if myName == 'Maria':
+    print('This is a very nice name, '+ myName)
+else:
+    print('This isn\'t a name, ' + myName)
 print('The length of your name is:' + str(len(myName)))
 print('What is your age?')
 myAge = input()
-print('You will be ' + str(myAge+1) + ' in a year. Maybe.')
+print('You will be ' + str(int(myAge)+1) + ' in a year. Maybe.')
