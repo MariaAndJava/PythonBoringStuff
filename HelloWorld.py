317d4612ace74ea5f465396676762eb66553cd3b
MariaAndJava <muszyns@cs.uni-bonn.de>
2019-11-05 11:26:10 +0100

    bullshit : )

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,1 +1,9 @@
-print('Hello World!')
+# This program says hello and asks for my name
+print('Hello world!')
+print('What is your name?') #ask for the name
+myName = input()
+print('This is a very nice name, '+ myName)
+print('The length of your name is:' + str(len(myName)))
+print('What is your age?')
+myAge = input()
+print('You will be ' + str(myAge+1) + ' in a year. Maybe.')
