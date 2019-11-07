ef6571f261c6fb189b1ffb68cc9f7e3d93ca3f5d
MariaAndJava <muszyns@cs.uni-bonn.de>
2019-11-07 12:01:30 +0100

    Manipulating the File System

--- FileStuff.py
+++ FileStuff.py
@@ -22,3 +22,20 @@ shelfFile.close()
 shelfFile = shelve.open('myData')
 for i in shelfFile.keys():
     pprint.pprint(shelfFile[i])
+
+import shutil, os, send2trash
+
+def copyMoveRenameFile():
+    shutil.copy('C:\\spam.txt', 'C:\\desktop\\misc')#from first folder to right folder
+    shutil.copy('file to copy', 'to this path with eg new name \\spam_2.txt')
+    shutil.copytree('see', 'above') #all subfolders and files
+    shutil.move('from', 'to')
+    shutil.move('to rename copy to the same folder','but with a new name')
+
+def removeDeleteFile():
+    shutil.rmtree('path') #removes non empty folders with all files
+    os.rmdir('path') #removes empty files
+    os.unlink('remove file')j
+
+def send2trashInsteadOfDeleting():
+    send2trash.send2trash('path\\file.txt')
